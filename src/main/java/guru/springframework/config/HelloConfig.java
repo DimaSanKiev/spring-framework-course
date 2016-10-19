6dcf3a8d8b56a17a5df641cae9d064de317e9df0
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-10-20 00:54:37 +0300

    Add advanced autowired options

--- src/main/java/guru/springframework/config/HelloConfig.java
+++ src/main/java/guru/springframework/config/HelloConfig.java
@@ -2,10 +2,9 @@ package guru.springframework.config;
 
 import guru.springframework.services.HelloWorldFactory;
 import guru.springframework.services.HelloWorldService;
-import guru.springframework.services.HelloWorldServiceEnglishImpl;
-import guru.springframework.services.HelloWorldServiceSpanishImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
@@ -18,30 +17,29 @@ public class HelloConfig {
 
     @Bean
     @Profile("english")
+    @Primary
     public HelloWorldService helloWorldServiceEnglish(HelloWorldFactory factory) {
         return factory.createHelloWorldService("en");
     }
 
     @Bean
     @Profile("spanish")
+    @Primary
     public HelloWorldService helloWorldServiceSpanish(HelloWorldFactory factory) {
         return factory.createHelloWorldService("es");
     }
 
-    @Bean
-    @Profile("french")
+    @Bean(name = "french")
     public HelloWorldService helloWorldServiceFrench(HelloWorldFactory factory) {
         return factory.createHelloWorldService("fr");
     }
 
     @Bean
-    @Profile("german")
     public HelloWorldService helloWorldServiceGerman(HelloWorldFactory factory) {
         return factory.createHelloWorldService("de");
     }
 
     @Bean
-    @Profile("ukrainian")
     public HelloWorldService helloWorldServiceUkrainian(HelloWorldFactory factory) {
         return factory.createHelloWorldService("ua");
     }
--- src/main/java/guru/springframework/controllers/GreetingController.java
+++ src/main/java/guru/springframework/controllers/GreetingController.java
@@ -2,6 +2,7 @@ package guru.springframework.controllers;
 
 import guru.springframework.services.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -9,14 +10,37 @@ public class GreetingController {
 
     private HelloWorldService helloWorldService;
 
+    private HelloWorldService helloWorldServiceGerman;
+
+    private HelloWorldService helloWorldServiceFrench;
+
+    // English version is injected by type
     @Autowired
     public void setHelloWorldService(HelloWorldService helloWorldService) {
         this.helloWorldService = helloWorldService;
     }
 
+    // German version is injected by name
+    @Autowired
+    @Qualifier("helloWorldServiceGerman")
+    public void setHelloWorldServiceGerman(HelloWorldService helloWorldServiceGerman) {
+        this.helloWorldService = helloWorldServiceGerman;
+    }
+
+    // German version is injected by name but with different configuration
+    @Autowired
+    @Qualifier("french")
+    public void setHelloWorldServiceFrench(HelloWorldService helloWorldServiceFrench) {
+        this.helloWorldService = helloWorldServiceFrench;
+    }
+
     public String sayHello() {
         String greeting = helloWorldService.getGreeting();
+
         System.out.println(greeting);
+        System.out.println(helloWorldServiceGerman.getGreeting());
+        System.out.println(helloWorldServiceFrench.getGreeting());
+
         return greeting;
     }
 }
