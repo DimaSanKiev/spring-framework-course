86c60878be6f7384da62d6a047611d396cc5f1fe
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-10-21 23:18:40 +0300

    Completely move to XML based configuration

--- src/main/java/guru/springframework/controllers/GreetingController.java
+++ src/main/java/guru/springframework/controllers/GreetingController.java
@@ -1,11 +1,7 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.HelloWorldService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
 
-@Controller
 public class GreetingController {
 
     private HelloWorldService helloWorldService;
@@ -14,22 +10,14 @@ public class GreetingController {
 
     private HelloWorldService helloWorldServiceFrench;
 
-    // English version is injected by type
-    @Autowired
     public void setHelloWorldService(HelloWorldService helloWorldService) {
         this.helloWorldService = helloWorldService;
     }
 
-    // German version is injected by name
-    @Autowired
-    @Qualifier("helloWorldServiceGerman")
     public void setHelloWorldServiceGerman(HelloWorldService helloWorldServiceGerman) {
         this.helloWorldServiceGerman = helloWorldServiceGerman;
     }
 
-    // German version is injected by name but with different configuration
-    @Autowired
-    @Qualifier("french")
     public void setHelloWorldServiceFrench(HelloWorldService helloWorldServiceFrench) {
         this.helloWorldServiceFrench = helloWorldServiceFrench;
     }
